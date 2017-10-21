96cfb931cc641d46a1c1bf8574da367603a90f1a
Bogdan Niculeasa <bogdan.niculeasa@hotmail.com>
2017-10-22 01:16:19 +0300

    =added dockerfiles for spark with cassandra

---
+++ Spark_2.1.0/WithCassandraConnector_2.0.0/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#set proxy to be used by spark when adding new packages
+echo "spark.driver.extraJavaOptions -Dhttp.proxyHost=87.254.212.120 -Dhttp.proxyPort=8080 -Dhttps.proxyHost=87.254.212.120 -Dhttps.proxyPort=8080" >>$SPARK_HOME/conf/spark-defaults.conf
+#add spark package for connecting to cassandra (it assumes Spark has already been installed and $SPARK_HOME already set
+#echo "spark.jars.packages datastax:spark-cassandra-connector:2.1.0-s_2.11" >> $SPARK_HOME/conf/spark-defaults.conf
+#add ip of the cassandra cluster no need if they are linked
+#echo "spark.cassandra.connection.host 172.17.0.3" >> $SPARK_HOME/conf/spark-defaults.conf
+exec "$@"
